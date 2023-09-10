ebbc00f1056c185ab7a67f0b0b303404500afe3e
Zhaoyi Zhou <zhouzhao20@mails.tsinghua.edu.cn>
2023-09-10 23:08:28 +0800

    Added data collection method

--- run.sh
+++ run.sh
@@ -1,8 +1,22 @@
-for env in 'halfcheetah' 'hopper' 'walker2d'
+# 'halfcheetah' 'hopper' 'walker2d'
+
+# for env in 'walker2d'
+# do
+#     pkl="./snapshots/${env}/params.pkl"
+#     out_dir="./dataset/${env}"
+#     mkdir -p out_dir
+#     out="${out_dir}//output.hdf5"
+#     python scripts/generation/mujoco/collect_data.py "${env}-medium-v2" --num_data=10000 --output_file=${out} --pklfile=${pkl} \
+#         --render
+# done
+
+for env in 'halfcheetah'
 do
     pkl="./snapshots/${env}/params.pkl"
     out_dir="./dataset/${env}"
     mkdir -p out_dir
     out="${out_dir}//output.hdf5"
-    python scripts/generation/mujoco/collect_data.py "${env}-medium-v2" --num_data=10000 --output_file=${out} --pklfile=${pkl}
+    python scripts/generation/mujoco/collect_data_v2.py "${env}-medium-v2" --num_data=10000 --output_file=${out} --pklfile=${pkl} \
+        --period 100 \
+        --render
 done
